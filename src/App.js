bc4d56f9a3a2a9bf6e08492210964d3669bdfb20
ProgramerNila <programernila@gmail.com>
2023-01-29 17:57:54 +0600

    update dom

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ import {
   Route,
 } from "react-router-dom";
 import RootLayout from "./components/RootLayout";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Registation from "./pages/Registation";
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route  element={<Registation/>}></Route>
+    /*  <Route path="/" element={<RootLayout />}>
       <Route index element={<Login/>}></Route>
+      <Route path="/registation"  element={<Registation/>}></Route>
+      <Route path="/home"  element={<Home/>}></Route>
+    </Route> */
+    <Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/registation" element={<Registation />}></Route>
+      <Route path="/home" element={<Home />}></Route>
     </Route>
   )
 );
--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 import Registation from "../pages/Registation";
 
 const RootLayout = () => {
   return (
     <>
+      <Login />
+      {/* <Outlet/> */}
       {/* <Registation /> */}
-      <Login/>
     </>
   );
 };
